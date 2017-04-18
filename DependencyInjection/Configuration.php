8afdc9a038eee8c8fea2da928042fee0b7d7a9bf
maxence d'Espeuilles <maxence@openaccess.fr>
2017-04-18 11:35:54 +0200

    fix services

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('open_access_mail');
+        $rootNode = $treeBuilder->root('mdespeuilles_mail');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
